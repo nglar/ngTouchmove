4a048a6a472acf0e7b065d029e03cf604feb5af2
Mark Topper <mail@webman.io>
2015-02-03 12:27:32 +0100

    Create ngTouchmove.js

---
+++ ngTouchmove.js
@@ -0,0 +1,28 @@
+"use strict";
+
+angular.module("ngTouchmove", []).directive("ngTouchmove", function () {
+  return {
+    controller: function ($scope, $element, $attrs) {
+      $element.bind('touchstart', onTouchStart);
+      
+      function onTouchStart(event) {
+        event.preventDefault();
+        $element.bind('touchmove', onTouchMove);
+        $element.bind('touchend', onTouchEnd);
+      };
+      
+      function onTouchMove(event) {
+          var method = '$scope.' + $element.context.getAttribute('ng-touchmove');
+          $scope.$apply(function () {
+              eval(method);
+          });
+      };
+      
+      function onTouchEnd(event) {
+        event.preventDefault();
+        $element.unbind('touchmove', onTouchMove);
+        $element.unbind('touchend', onTouchEnd);
+      };
+    }
+  };
+});
